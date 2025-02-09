6ed77f010afc1fcf2350f2e9e34730c0b4180ebc
Harsh Kumar <harshj7.net@gmail.com>
2025-02-10 04:50:59 +0530

    proper meta tags and schema markup

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -3,6 +3,9 @@ import InputSearch from './InputSearch'
 const Hero = () => {
     return (
         <div className='hero flex justify-center items-center relative'>
+            <h1 className='text-white font-bold text-2xl'>
+                Movie Mingle - The Ultimate Movie Explorer
+            </h1>
             <InputSearch />
         </div>
     )
